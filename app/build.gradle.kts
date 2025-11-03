72724818923bbb094a894ff6aa43f872675e1f79
Ragavendra Nagraj <ragavendra.bn@gmail.com>
2025-11-03 10:30:01 -0800

    move to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.jyothishaastra.DegMinSec"
+    mainClass = "org.jyothishaastra.App"
 }
 
 tasks.named<Test>("test") {
